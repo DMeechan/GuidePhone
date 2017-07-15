33dc3e2b85c92cdf187cb24eac03af0ee947bb90
DMeechan <daniel.meechan.1+git@gmail.com>
2017-07-15 17:46:41 +0100

    Moved old project

---
+++ Guardphone/app/src/main/java/guardphone/com/guardphone/ConnectingActivity.java
@@ -0,0 +1,13 @@
+package guardphone.com.guardphone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConnectingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connecting);
+    }
+}
